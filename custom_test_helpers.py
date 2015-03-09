e32f897c0aa213a4b25b445ebc477c0191b80609
Tal Einat <taleinat@gmail.com>
2015-03-09 10:25:14 +0200

    fixed @inspect_assertions

--- custom_test_helpers.py
+++ custom_test_helpers.py
@@ -54,21 +54,21 @@ def check_tests_fail(module, error_text="All tests passed on a bad implementatio
 
 
 def _get_assertion_method_names(class_):
-    return (
+    return [
         name for name in dir(class_)
         if name.startswith(("assert", "failIf", "failUnless"))
         and "_" not in name
         and isinstance(getattr(class_, name), types.FunctionType)
-    )
+    ]
 ASSERTION_METHOD_NAMES = frozenset(_get_assertion_method_names(unittest.TestCase))
 
 
 def get_test_method_names(class_):
-    return (
+    return [
         name for name in dir(class_)
         if (name == 'runTest' or name.startswith("test"))
         and isinstance(getattr(class_, name), types.FunctionType)
-    )
+    ]
 
 
 def _override_method_with_mock(test_case_instance, method_name):
@@ -110,44 +110,43 @@ def inspect_assertions(test_case_class):
         counter_method = unittest.mock.Mock(return_value=None)
         class_counters[assertion_method_name] = counter_method
 
+    per_method_counters = {}
+    test_case_class.per_method_counters = per_method_counters
     for test_method_name in test_method_names:
+        per_method_counters[test_method_name] = method_counters = {}
+        for assertion_method_name in ASSERTION_METHOD_NAMES:
+            counter_method = unittest.mock.Mock(return_value=None)
+            method_counters[assertion_method_name] = counter_method
+
+    def make_wrapper(orig_method, test_method_name, assertion_method_name):
+        @functools.wraps(orig_method)
+        def wrapper(*args_, **kwargs_):
+            class_counters[assertion_method_name](*args_, **kwargs_)
+            per_method_counters[test_method_name][assertion_method_name](*args_, **kwargs_)
+            return orig_method(*args_, **kwargs_)
+        return wrapper
+
+    for test_method_name in test_method_names:
+        def make_new_test_method(test_method_name, orig_test_method):
+            @functools.wraps(orig_test_method)
+            def new_test_method(self, *args, **kwargs):
+                orig_methods = {}
+                for assertion_method_name in ASSERTION_METHOD_NAMES:
+                    orig_method = getattr(self, assertion_method_name)
+                    orig_methods[assertion_method_name] = orig_method
+                    wrapper = make_wrapper(orig_method, test_method_name, assertion_method_name)
+                    setattr(self, assertion_method_name, wrapper)
+
+                try:
+                    return orig_test_method(self, *args, **kwargs)
+                finally:
+                    for assertion_method_name, orig_method in orig_methods.items():
+                        setattr(self, assertion_method_name, orig_method)
+
+            return new_test_method
+
         orig_test_method = getattr(test_case_class, test_method_name)
-
-        @functools.wraps(orig_test_method)
-        def new_test_method(self, *args, **kwargs):
-            orig_methods = {}
-            new_methods = {}
-            for assertion_method_name in ASSERTION_METHOD_NAMES:
-                orig_method = getattr(self, assertion_method_name)
-                orig_methods[assertion_method_name] = orig_method
-                def make_wrapper(orig_method, assertion_method_name):
-                    @functools.wraps(orig_method)
-                    def wrapper(*args, **kwargs):
-                        class_counters[assertion_method_name](*args, **kwargs)
-                        return orig_method(*args, **kwargs)
-                    return wrapper
-                wrapper = make_wrapper(orig_method, assertion_method_name)
-                mock_method = unittest.mock.Mock(wraps=wrapper)
-                setattr(self, assertion_method_name, mock_method)
-                new_methods[assertion_method_name] = mock_method
-
-            new_test_method.wrapped_methods = new_methods
-
-            def get_total_call_count(method_names=None):
-                if method_names is None:
-                    method_names = ASSERTION_METHOD_NAMES
-                return sum(
-                    new_methods[method_name].call_count
-                    for method_name in method_names
-                )
-            new_test_method.get_total_call_count = get_total_call_count
-
-            try:
-                return orig_test_method(self, *args, **kwargs)
-            finally:
-                for assertion_method_name, orig_method in orig_methods.items():
-                    setattr(self, assertion_method_name, orig_method)
-
+        new_test_method = make_new_test_method(test_method_name, orig_test_method)
         setattr(test_case_class, test_method_name, new_test_method)
 
     @classmethod
@@ -159,6 +158,16 @@ def inspect_assertions(test_case_class):
             for method_name in method_names
         )
     test_case_class.get_total_call_count = get_total_call_count
+
+    @classmethod
+    def get_test_method_total_call_count(cls, test_method_name, method_names=None):
+        if method_names is None:
+            method_names = ASSERTION_METHOD_NAMES
+        return sum(
+            cls.per_method_counters[test_method_name][method_name].call_count
+            for method_name in method_names
+        )
+    test_case_class.get_test_method_total_call_count = get_test_method_total_call_count
 
     return test_case_class
 
